20f578f731dcc96b11e16e74d08bf9fd2a581a27
Aslannymous <aslanhuseynov9@gmail.com>
2023-03-27 22:07:52 +0400

    Insert

--- Library.Persistance/Implementations/LibraryRepository.cs
+++ Library.Persistance/Implementations/LibraryRepository.cs
@@ -1,6 +1,29 @@
-﻿namespace Library.Persistance.Implementations
+﻿using AutoMapper;
+using Library.Domain.Models;
+using Library.Persistance.DB;
+using WebApiProject.Repositories.BookRepository;
+using WebApiProject.Repositories.LibraryRepository;
+
+namespace Library.Persistance.Implementations
 {
-    public class LibraryRepository
+    public class LibraryRepository : ILibraryRepository
     {
+        private readonly IMapper _mapper;
+        private IBookRepository _bookRepository;
+        public LibraryRepository(IBookRepository bookRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _bookRepository = bookRepository;
+        }
+
+        public Task<Book> Borrow(int bookId)
+        {
+            _bookRepository.GetBook();
+        }
+
+        public Task<Book> Return(int bookId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
